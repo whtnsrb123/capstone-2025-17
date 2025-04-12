ba8deaaf5d446b416ec4d98d94714e0852cc28b1
MsKing <dasung56@gmail.com>
2025-04-13 00:41:30 +0900

    [FEAT] 플레이어가 물체 잡는 동작 동기화
    잡을 수 있는 물체의 photonView에 ownerShipTransfer도 Request로 변경하였습니다.

--- Assets/Scripts/Player/CameraOnOff.cs
+++ Assets/Scripts/Player/CameraOnOff.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class CameraOnOff : MonoBehaviourPun
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         if (photonView.IsMine)
         {
