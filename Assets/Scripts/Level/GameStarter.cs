758c74f76f1959aa57d13ebbd26f461aad1ce66e
daesung <dasung56@gmail.com>
2025-04-29 17:33:15 +0900

    [FEAT] 로비씬에 ServerTestOnOff 버튼 추가

--- Assets/Scripts/Level/GameStarter.cs
+++ Assets/Scripts/Level/GameStarter.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameStarter : MonoBehaviourPun
 {
     public static void GameStart()
     {
-        PhotonNetwork.LoadLevel("Mission1");
+        if (GameStateManager.isServerTest)
+        {
+            PhotonNetwork.LoadLevel("Mission1");
+        }
+        else
+        {
+            SceneManager.LoadScene("Mission1");
+        }
     }
 }
