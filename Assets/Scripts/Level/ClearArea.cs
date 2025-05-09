0912164b7936719455e4cb093087ccf80e063de6
Woojin Kwak <kwooj2788@kookmin.ac.kr>
2025-05-10 00:40:17 +0900

    [FEAT] 클리어 구역을 Line Renderer로 시각화하는 기능 구현

--- Assets/Scripts/Level/ClearArea.cs
+++ Assets/Scripts/Level/ClearArea.cs
@@ -8,6 +8,45 @@ public class ClearArea : MonoBehaviour
     [SerializeField]
     private int clearCount = 0;
 
+    private BoxCollider collider;
+    private LineRenderer line;
+
+    void Start( )
+    {
+        collider = GetComponent<BoxCollider>();
+
+        line = gameObject.AddComponent<LineRenderer>();
+        line.positionCount = 5;
+        line.loop = false;
+        line.useWorldSpace = false;
+        line.widthMultiplier = 0.05f;
+        line.material = new Material(Shader.Find("Sprites/Default"));
+        line.startColor = line.endColor = Color.cyan;
+
+        DrawRectangle();
+    }
+    
+    private void DrawRectangle()
+    {
+        Vector3 size = collider.size;
+        Vector3 center = collider.center;
+
+        float halfX = size.x / 2;
+        float halfZ = size.z / 2;
+        float fixedY = center.y - 0.4f;
+
+        Vector3[] corners = new Vector3[5]
+        {
+            new Vector3(center.x - halfX, fixedY, center.z + halfZ),
+            new Vector3(center.x + halfX, fixedY, center.z + halfZ),
+            new Vector3(center.x + halfX, fixedY, center.z - halfZ),
+            new Vector3(center.x - halfX, fixedY, center.z - halfZ),
+            new Vector3(center.x - halfX, fixedY, center.z + halfZ)
+        };
+
+        line.SetPositions(corners);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
