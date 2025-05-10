07c524f788a6977f82904de4510bab568efc07d7
daesung <dasung56@gmail.com>
2025-05-10 15:15:55 +0900

    [FIX]입력 꼬임 수정

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -1,12 +1,15 @@
+using Photon.Pun;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class CameraFollow : MonoBehaviourPun
 {
     public Transform cameraMount;
 
-    void Start()
+    void Awake()
     {
         Camera mainCam = Camera.main;
+        if (GameStateManager.isServerTest && !photonView.IsMine)
+            return;
         mainCam.transform.SetParent(cameraMount); // 카메라를 플레이어에 붙이고
         mainCam.transform.localPosition = Vector3.zero; // 위치 초기화
         mainCam.transform.localRotation = Quaternion.identity; // 회전 초기화
