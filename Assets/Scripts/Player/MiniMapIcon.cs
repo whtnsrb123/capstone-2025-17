a9ea3f40f40280edff2d9cc574e7db9079623dd9
daesung <dasung56@gmail.com>
2025-05-18 14:44:40 +0900

    [FIX] 플레이어 미니맵 아이콘 수정

---
+++ Assets/Scripts/Player/MiniMapIcon.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapIcon : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.rotation = Quaternion.identity;
+    }
+}
