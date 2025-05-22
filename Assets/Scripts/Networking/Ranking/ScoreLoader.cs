d44d8115549787d8ed851505f64b5621adffea6a
JiyeonJeong01 <jizzvibe@gmail.com>
2025-05-22 11:23:31 +0900

    랭킹 로드 전 로딩 UI 추가

--- Assets/Scripts/Networking/Ranking/ScoreLoader.cs
+++ Assets/Scripts/Networking/Ranking/ScoreLoader.cs
@@ -5,10 +5,19 @@ using TMPro;
 using UnityEngine;
 using System.Linq;
 using JetBrains.Annotations;
+using UnityEngine.UI;
 
 public class ScoreLoader : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI[] infoTMPs;
+    [SerializeField] GameObject loadingImage;
+
+    PopupUI popupUI;
+
+    private void Start()
+    {
+        popupUI = GetComponent<PopupUI>();
+    }
 
     public void InvokeLoadRanking()
     {
@@ -21,8 +30,6 @@ public class ScoreLoader : MonoBehaviour
     {
         List<Dictionary<string, object>> topFive =  await LeaderboardManager.Instance.GetAllLeaderboardData();
 
-        Debug.Log("================================================");
-
         int index = 0;
         foreach (var row in topFive)
         {
@@ -30,14 +37,21 @@ public class ScoreLoader : MonoBehaviour
             string[] players = ((List<object>)row["players"]).Select(p => p.ToString()).ToArray();
 
             infoTMPs[index++].text = $"{time:F2}초\t\t{string.Join(", ", players)}";
+
+            loadingImage.SetActive(false);
         }
 
 
     }
 
+    public void OnClickRankingboard()
+    {
+        loadingImage.SetActive(true);
+        popupUI.ShowUI();
+    }
 
-
-
-
-
+    public void OnClickCloseBtn()
+    {
+        popupUI.HideUI();
+    }
 }
