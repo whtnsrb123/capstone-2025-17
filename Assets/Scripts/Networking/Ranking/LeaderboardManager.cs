bec6a7dbb7ed89202234cb6c89a3fddadfc2811b
JiyeonJeong01 <jizzvibe@gmail.com>
2025-05-22 11:03:17 +0900

    LeaderboardManager.cs 씬 전환 시 파괴되도록 설정

--- Assets/Scripts/Networking/Ranking/LeaderboardManager.cs
+++ Assets/Scripts/Networking/Ranking/LeaderboardManager.cs
@@ -43,7 +43,6 @@ public class LeaderboardManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
